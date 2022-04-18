dd3ea39349f2c1fed25add28a238f7ad40b4081d
Victor2706 <vmanoel1727@gmail.com>
2022-04-18 12:06:46 -0300

    Novos Arquivos

---
+++ Alunos/alunos.js
@@ -0,0 +1,62 @@
+const data = [     
+    {
+        id: 1,
+        nome: 'Ana',
+        sexo: 'Feminino',
+        nota_1_bim: 7,
+        nota_2_bim: 4,
+        aprovado: true,
+    },
+    {
+        id: 2,
+        nome: 'Beto',
+        sexo: 'Masculino',
+        nota_1_bim: 5,
+        nota_2_bim: 4,
+        aprovado: false,
+    },
+    {
+        id: 3,
+        nome: 'Carla',
+        sexo: 'Feminino',
+        nota_1_bim: 8,
+        nota_2_bim: 6,
+        aprovado: true,
+    },
+    {
+        id: 4,
+        nome: 'Don',
+        sexo: 'Masculino',
+        nota_1_bim: 4,
+        nota_2_bim: 4,
+        aprovado: false,
+    },
+];
+
+function cadastrar(aluno) {
+
+}
+
+function listar() {
+
+}
+
+function listarPorId(id) {
+
+}
+
+function listarAprovados() {
+
+}
+
+function listarReprovados() {
+    
+}
+
+module.exports = {
+    cadastrar,
+    listar,
+    listarPorId,
+    listarAprovados,
+    listarReprovados
+}
